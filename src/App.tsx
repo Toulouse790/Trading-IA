180368de5175f7822c96ad7a767421fda39c7a73
Toulouse790 <cstophe0@gmail.com>
2025-06-25 16:36:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardIA from "./pages/DashboardIA";
+import AgentDetailsPage from "./pages/AgentDetailsPage"; // Importation de la nouvelle page de détails
+import AssistantManagement from "./pages/AssistantManagement"; // Importation de la page de gestion des assistants
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashboardIA />} />
+          <Route path="/agent/:agentId" element={<AgentDetailsPage />} /> {/* Nouvelle route pour les détails de l'agent */}
+          <Route path="/assistants" element={<AssistantManagement />} /> {/* Route pour la gestion des assistants */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
